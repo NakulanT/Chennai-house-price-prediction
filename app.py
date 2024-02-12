185330630c439e36f0eb947cb49f468e31beeea6
NakulanT <nakult721@gmail.com>
2024-02-12 21:15:52 +0530

    Added gitignore

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 def index():
     result = None
     if request.method == 'POST':
-        area = int(request.form['area'])
-        int_sqft = int(request.form['int_sqft'])
-        dist_mainroad = int(request.form['dist_mainroad'])
-        n_bedroom = int(request.form['n_bedroom'])
-        n_bathroom = int(request.form['n_bathroom'])
-        n_room = int(request.form['n_room'])
-        park_facil = int(request.form['park_facil'])
-        result = predict(area , int_sqft ,dist_mainroad,n_bedroom ,n_bathroom ,n_room,park_facil)
-    return render_template('index.html', result=result)
+        area = request.form['area']
+        int_sqft = request.form['int_sqft']
+        dist_mainroad = request.form['dist_mainroad']
+        n_bedroom = request.form['n_bedroom']
+        n_bathroom = request.form['n_bathroom']
+        n_room = request.form['n_room']
+        park_facil = request.form['park_facil']
+        if area and int_sqft and dist_mainroad and n_bathroom and n_bedroom and n_room:
+            result = predict(area , int_sqft ,dist_mainroad,n_bedroom ,n_bathroom ,n_room,park_facil)
+            return render_template('index.html', result= int(result))
+        return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
